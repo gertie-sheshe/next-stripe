600a9277ffeb502917f4965883add738a25e5bcc
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-06-01 10:29:35 +0300

    (fix) move lib folder

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import { checkout } from "./lib/stripe/checkout";
+import { checkout } from "../lib/stripe/checkout";
 
 export default function Home() {
   return (
